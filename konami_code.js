feb330004b16966cfd0a4aee3c093ea9ee6a96ad
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-04 04:57:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,5 +6,6 @@ function init() {
   // Write your JavaScript code inside the init() function
   //what's listening (document.body)
   // check for 'keydown' events
+  // key = alaphabet[index]
   
 }
