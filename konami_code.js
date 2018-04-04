4b18e97928570f78fc9015dc9f9ae7626ad80ccb
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-04 05:42:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   //what's listening (document.body)
-  let Input = document.body
+  let input = document.body
   
   input.addEventListener('keydown', function(e) {
   
