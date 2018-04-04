8ff930bacbbc87b02432ea8324946f89797c29ec
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-04 06:28:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ function init() {
     if (key === code[index]) {
       index++
       if (index === code.length) {
-        alert("Hurray!")
+        window.alert("Hurray!")
         index = 0
       }
     } else {
